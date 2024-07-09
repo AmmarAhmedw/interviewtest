89a623b2f8276b280172ecad8f90666860074df6
AmmarAhmadww <ammarahmed@punch.agency>
2024-07-09 22:02:24 +0500

    new commit

--- interview/src/app/page.tsx
+++ interview/src/app/page.tsx
@@ -1,87 +1,113 @@
-import React from 'react';
+import Image from "next/image";
 
-const InterviewSettings: React.FC = () => {
+export default function Home() {
   return (
-    <div className="interview-settings-container"> 
-      <div className="interview-settings">
-        <h2 className="Interview1">Interview Settings</h2>
-        <p className="description1">
-          We may still send you important notifications about your account outside of your notification settings.
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
-        <hr className="line"></hr>
-        <div className="setting">
-          <label className="setting-title1">
-            Follow on Questions
-          </label>
-          <p className="setting-description " style={{ fontFamily: 'Switzer' }}>
-            Follow-up questions are <b>AI Based </b> additional questions that build upon or explore further the response to an initial question.
-          </p>
-          <label className="question" style={{ fontFamily: 'Switzer' }}>
-            <input type="checkbox" /> Enable Follow on Questions
-          </label>
-        </div>
-        <hr className="line01"></hr>
-        <div className="setting">
-          <label className="setting-title2">
-            Question per Category
-          </label>
-          <p className="setting-description" style={{ fontFamily: 'Switzer' }}>
-            These are AI generated questions. The interview will be divided into 4 main categories.
-          </p>
-          <div className="ml-24">
-          <select className="border border-gray-300 rounded-xl w-full sm:w-3/4 md:w-1/4 lg:w-1/6 xl:w-1/8 h-12 mt-[-70px] flex mx-auto md:flex-row md:justify-center">
-
-            <option>Select option</option>
-            <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
-        <option value="4">Option 4</option>
-        <option value="5">Option 5</option>
-          </select></div>
-        </div>
-        <div className="setting">
-        <hr className="line02"></hr>
-          <label className="setting-title3">
-    
-            Set Question Delay
-          </label>
-          <p className="setting-description" style={{ fontFamily: 'Switzer' }}>
-            These are AI generated questions. The interview will be divided into 4 main categories.
-          </p>
-          <div className="ml-24">
-          <select className="border border-gray-300 rounded-xl w-full sm:w-3/4 md:w-1/4 lg:w-1/6 xl:w-1/8 h-12 mt-[-70px] flex mx-auto md:flex-row md:justify-center">
-            <option className="fixed">Select option</option>
-            <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
-        <option value="4">Option 4</option>
-        <option value="5">Option 5</option>
-          </select>
-
-          </div>
-        </div>
-        <div className="setting">
-          <hr className="line03"/> 
-          <label className="setting-title4">
-            Show Interview Statistics
-          </label>
-          <p className="setting-description" style={{ fontFamily: 'Switzer' }}>
-            These are AI generated questions. The interview will be divided into 4 main categories.
-          </p>
-          <div>
-            <div className="yesno" style={{ fontFamily: 'Switzer' }}>
-            <label className="yes flex justify-center sm:ml-[-4%] sm:mr-2 -mt-20">
-              <input type="radio" name="stats" value="yes" /> Yes
-            </label> </div>
-          <label className="no flex justify-center ml-0 sm:ml-[25%] sm:mr-32 -mt-6" style={{ fontFamily: 'Switzer' }}>
-              <input type="radio" name="stats" value="no" /> No
-            </label> 
-            </div>
-          </div>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
         </div>
       </div>
 
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Explore starter templates for Next.js.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
   );
 }
-
-export default InterviewSettings;
